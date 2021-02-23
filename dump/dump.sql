e86ca4e2d82b434faab4a83897c397d50397ed40
Cansın Aldanmaz <canald@gmail.com>
2021-02-24 02:03:20 +0300

    Add some sample tables and data

--- dump/dump.sql
+++ dump/dump.sql
@@ -1,24 +1,86 @@
 ALTER USER 'ceph' IDENTIFIED WITH mysql_native_password BY 'cephPass';
-
 CREATE TABLE EMM_KullandikcaOde_Ozet (
-  Tarih int(11) PRIMARY KEY,
-  ToplamKayit int(11),
-  ToplamKullanimByte int(11)
+  Tarih INT(8) NOT NULL PRIMARY KEY,
+  ToplamKayit INT NOT NULL,
+  ToplamKullanimByte INT NOT NULL
 );
-
 CREATE TABLE EMM_KullandikcaOde_Detay (
-  Tarih int(11),
-  MusteriNo int(11),
-  Lokasyon text(20),
-  ErisimTipi text(20),
-  SiparisNo int(20),
-  ServisTipi text(20),
-  KullanimByte text(30)
+  ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  Tarih INT(8) NOT NULL,
+  KullanimByte INT NOT NULL,
+  ServisTipi TEXT NOT NULL,
+  SiparisNo TEXT NOT NULL,
+  MusteriNo TEXT NOT NULL,
+  Erisimtipi TEXT NOT NULL
 );
-
-INSERT INTO EMM_KullandikcaOde_Ozet 
-(Tarih, ToplamKayit, ToplamKullanimByte)
-VALUES 
-(20210221, 6, 50),
-(20210220, 2, 100)
-;
+INSERT INTO
+  EMM_KullandikcaOde_Ozet (Tarih, ToplamKayit, ToplamKullanimByte)
+VALUES
+  (20210221, 6, 50),
+  (20210220, 2, 100);
+INSERT INTO
+  EMM_KullandikcaOde_Detay (
+    Tarih,
+    KullanimByte,
+    ServisTipi,
+    SiparisNo,
+    MusteriNo,
+    Erisimtipi
+  )
+VALUES
+  (
+    20210224,
+    4096,
+    "READ",
+    "xyz12354617",
+    "mus0002",
+    "LOCAL"
+  ),
+  (
+    20210224,
+    10240,
+    "CAPACITY",
+    "xyz12354617",
+    "mus0001",
+    "LOCAL"
+  ),
+  (
+    20210224,
+    1024,
+    "CAPACITY",
+    "xyz12354617",
+    "mus0003",
+    "INTERNET"
+  ),
+  (
+    20210223,
+    1024,
+    "READ",
+    "xyz12354617",
+    "mus0001",
+    "INTERNET"
+  ),
+  (
+    20210223,
+    1024,
+    "CAPACITY",
+    "xyz12351237",
+    "mus0002",
+    "LOCAL"
+  ),
+  (
+    20210222,
+    1024,
+    "READ",
+    "xyz12354617",
+    "mus0001",
+    "LOCAL"
+  ),
+  (
+    20210221,
+    1024,
+    "CAPACITY",
+    "xyz1237",
+    "mus0004",
+    "INTERNET"
+  );
